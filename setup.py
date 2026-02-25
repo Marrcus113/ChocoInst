66e74ff77fcdd753a40a4a27f170df1d999b5d8f
Marrcus113 <deepseek748@gmail.com>
2026-02-26 02:53:14 +0300

    Bump version from 1.0.0 to 1.0.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="chocoinst",
-    version="1.0.0",
+    version="1.0.1",
     author="Марк",
     author_email="",
     description="🍫 ChocoInst - установка пакетов через Chocolatey с графическим интерфейсом",
